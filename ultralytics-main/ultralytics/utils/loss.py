92ffb9ba15b24572b6c8647c7c3569c07841dfbf
nithoukhan1 <inayatdms@yahoo.com>
2026-06-15 14:04:31 +0800

    loss.py updated

--- ultralytics-main/ultralytics/utils/loss.py
+++ ultralytics-main/ultralytics/utils/loss.py
@@ -128,7 +128,8 @@ class BboxLoss(nn.Module):
     ) -> tuple[torch.Tensor, torch.Tensor]:
         """Compute IoU and DFL losses for bounding boxes."""
         weight = target_scores.sum(-1)[fg_mask].unsqueeze(-1)
-        iou = bbox_iou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, CIoU=True)
+        from ultralytics.utils.focaler_iou import focaler_ciou
+        iou = focaler_ciou(pred_bboxes[fg_mask], target_bboxes[fg_mask], d=0.0, u=0.95)
         loss_iou = ((1.0 - iou) * weight).sum() / target_scores_sum
 
         # DFL loss
