ccc32fc44e657738d151adce9c8ddbbb374a9094
nithoukhan1 <inayatdms@yahoo.com>
2026-07-14 21:02:08 +0800

    Log MCAux loss in training results

--- ultralytics-main/ultralytics/models/yolo/detect/train.py
+++ ultralytics-main/ultralytics/models/yolo/detect/train.py
@@ -201,9 +201,26 @@ class DetectionTrainer(BaseTrainer):
 
     def get_validator(self):
         """Return a DetectionValidator for YOLO model validation."""
-        self.loss_names = "box_loss", "cls_loss", "dfl_loss"
+
+        if mcaux_enabled():
+            self.loss_names = (
+                "box_loss",
+                "cls_loss",
+                "dfl_loss",
+                "mcaux_loss",
+            )
+        else:
+            self.loss_names = (
+                "box_loss",
+                "cls_loss",
+                "dfl_loss",
+            )
+
         return yolo.detect.DetectionValidator(
-            self.test_loader, save_dir=self.save_dir, args=copy(self.args), _callbacks=self.callbacks
+            self.test_loader,
+            save_dir=self.save_dir,
+            args=copy(self.args),
+            _callbacks=self.callbacks,
         )
 
     def label_loss_items(self, loss_items: list[float] | None = None, prefix: str = "train"):
